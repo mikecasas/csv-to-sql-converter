517f492f22c9b44ba69ebd11b1849b233cd6b184
Mike Casas <mike.casas.public@gmail.com>
2021-05-27 09:28:43 -0400

    Improved to read files from a folder.

--- Program.cs
+++ Program.cs
@@ -10,18 +10,34 @@ namespace CsvToSqlConverter
         {
             var config = new FileUploadConfig();
 
-            config.DatabaseName = "Manbo";
-            config.FileName = "Feb-2020.csv";
-            config.FolderName = "C:\\Users\\mc\\desktop\\Manbo\\Manbo.Backend\\SqlScripts\\Data";
-            config.TableName = "FebTemp";
+            //config.FolderName = "C:\\Users\\mcasas\\Documents\\Munis-Financials\\csv";
 
+            config.FolderName = "C:\\Users\\mcasas\\Desktop\\other\\covid-19-public-safety\\May-2021\\csv";
+
+
+            config.DatabaseName = "DBScppSpecialProjects";
             config.IncludeDropCreateTable = true;
 
             var B = new SqlContentBuilder();
 
-            string content = B.BuildCreateTable(config);
+            //string[] files = { "apr-budget", "apr-actual" };
 
-            await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName + Guid.NewGuid().ToString()}.sql", content);
+            
+            //Option 1. List the files your self.
+            //string[] files = { "February.csv", "March.csv"};
+
+            //Option 2. Read them all from a folder
+            string[] files = Directory.GetFiles(config.FolderName);
+
+            foreach (string i in files)
+            {
+                config.FileName = Path.GetFileName(i); // Not needed + ".csv";
+                config.TableName = Path.GetFileNameWithoutExtension(i).Replace(" ", "-");
+
+                string content = B.BuildCreateTable(config);
+
+                await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName + "-" + Guid.NewGuid().ToString()}.sql", content);
+            }
 
             Console.WriteLine("Processing Complete!");
         }
