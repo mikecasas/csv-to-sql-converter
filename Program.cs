3d0ea000f3fea506c03ae7691b063db8c45578d4
Mike Casas <mike.casas.public@gmail.com>
2021-07-14 07:46:32 -0400

    Updates.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CsvToSqlConverter
 {
@@ -12,7 +14,10 @@ namespace CsvToSqlConverter
 
             string[] files = GetFiles(config);
 
-            await ProcessFiles(files, config);
+            var FilesProcessed = await ProcessFiles(files, config);
+            
+            string Last = CreateScriptFile(FilesProcessed, config.FolderName);
+            await File.WriteAllTextAsync($"{config.FolderName}\\00-run-all.sql", Last);
 
             Console.WriteLine("Processing Complete!");
         }
@@ -23,7 +28,9 @@ namespace CsvToSqlConverter
 
             //config.FolderName = "C:\\Users\\mcasas\\Documents\\Munis-Financials\\csv";
             //config.FolderName = "C:\\Users\\mcasas\\Desktop\\other\\covid-19-public-safety\\May-2021\\csv";
-            config.FolderName = "C:\\Users\\mcasas\\Documents\\_energov\\LBTR";
+            //config.FolderName = "C:\\Users\\mcasas\\Documents\\_energov\\LBTR";
+
+            config.FolderName = "C:\\Users\\mcasas\\Desktop\\test";
 
             //config.FolderName = "C:\\Users\\mcasas\\Downloads";
 
@@ -37,20 +44,42 @@ namespace CsvToSqlConverter
             return config;
         }
 
-        private static async Task ProcessFiles(string[] files, FileUploadConfig config)
+        private static async Task<IEnumerable<string>> ProcessFiles(string[] files, FileUploadConfig config)
         {
             var B = new SqlContentBuilder();
+            var ProcessedFiles = new List<string>();
 
             foreach (string i in files)
             {
+                long limit = 25000000; //25 Megs
+
+                FileInfo fi = new FileInfo(i);
+                long size = fi.Length;
+
                 config.FileName = Path.GetFileName(i); // Not needed + ".csv";
                 config.TableName = Path.GetFileNameWithoutExtension(i).Replace(" ", "-");
 
-                string content = B.BuildCreateTable(config);
+                if (size > limit)
+                {
+                    ProcessedFiles.AddRange(await B.HandleBigFile(config));
 
-                //await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName + "-" + Guid.NewGuid().ToString()}.sql", content);
-                await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName}.sql", content);
+                    //string content = B.BuildCreateTable(config);
+                    //await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName}.sql", content);
+
+                    //ProcessedFiles.Add(config.TableName);
+
+
+                } else
+                {               
+
+                    string content = B.BuildCreateTable(config);
+                    await File.WriteAllTextAsync($"{config.FolderName}\\{config.TableName}.sql", content);
+
+                    ProcessedFiles.Add(config.TableName);
+                }
             }
+
+            return ProcessedFiles;
         }
 
         /// <summary>
@@ -69,5 +98,20 @@ namespace CsvToSqlConverter
 
             return files;
         }
+
+        private static string CreateScriptFile(IEnumerable<string> files, string folder)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            sb.AppendLine($":SETVAR BasePath \"{folder}\\\"");
+
+            foreach (string f in files)
+            {
+                string fn = Path.GetFileName(f);
+                sb.AppendLine($":r $(BasePath){fn}.sql");
+            }
+
+            return sb.ToString();
+        }      
     }
 }
